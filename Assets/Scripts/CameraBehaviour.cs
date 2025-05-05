b1a133882c2372b9dec9521e8b7dee5168ae8ab8
AK42 <akopera@student.42.fr>
2025-05-06 00:50:34 +0200

    Removed camera smoothing
    This is a temporary fix to sprites blinking / flickering at low speeds.

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraBehaviour : MonoBehaviour {
+public class CameraBehaviour : MonoBehaviour
+{
 
     private Vector2 velocity;
-    public float smoothTimeY;
-    public float smoothTimeX;
+    //public float smoothTimeY;
+    //public float smoothTimeX;
 
     public GameObject player;
 
@@ -13,22 +14,31 @@ public class CameraBehaviour : MonoBehaviour {
 
     public Vector3 minCameraPos;
     public Vector3 maxCameraPos;
-    void Start ()
+
+    void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-	}
-    void FixedUpdate()
+    }
+
+    void LateUpdate()
     {
-        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+        // Smoothly follow the player
+        // Currently disabled because of sprite blinking
+
+        // Follow the player
+        float posX = player.transform.position.x;
+        float posY = player.transform.position.y;
 
         transform.position = new Vector3(posX, posY, transform.position.z);
 
+        // Apply camera bounds if enabled
         if (bounds)
         {
-            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
-                                             Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
-                                             Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+            transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z)
+            );
         }
-    }	
+    }
 }
